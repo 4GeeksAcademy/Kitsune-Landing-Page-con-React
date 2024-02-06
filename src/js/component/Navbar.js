c90afeb48090374621558fd5b26e1aaa06fdf57a
Diana PF <149829535+KitsuneDai@users.noreply.github.com>
2024-02-06 22:05:32 +0000

    add navbar

---
+++ src/js/component/Navbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark ps-5">
+    <div className="container-fluid">
+        <a className="navbar-brand">Start Bootstrap</a>
+        <div className="collapse navbar-collapse justify-content-end pe-5" id="navbarNavDropdown">
+        <ul className="navbar-nav">
+            <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+            <a className="nav-link" href="#">About</a>
+            </li>
+            <li className="nav-item">
+            <a className="nav-link" href="#">Services</a>
+            </li>
+            <li className="nav-item">
+            <a className="nav-link" href="#">Contact</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+    </nav>  
+        </>
+    )
+};
+
+export default Navbar
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import Navbar from "./Navbar";
 
 //create your first component
 const Home = () => {
 	return (
+		
 		<div className="text-center">
+			<Navbar/>
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
 			<p>
 				<img src={rigoImage} />
