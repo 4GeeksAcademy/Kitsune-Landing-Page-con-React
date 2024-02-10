cad13351341c77cec4f2251920db0db2fecf9f4c
Diana PF <149829535+KitsuneDai@users.noreply.github.com>
2024-02-10 23:16:33 +0000

    ultimo retoque!

--- src/js/component/Cards.js
+++ src/js/component/Cards.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Card from './Card'
-
 import firstProjectPhoto from '../../img/cat.jpg';
 import secondProjectPhoto from '../../img/mouse.jpg';
 import thirdProjectPhoto from '../../img/squirrel.jpg';
@@ -9,19 +8,17 @@ import fourtgProjectPhoto from "../../img/sunset.jpg";
 
 const Cards = () => {
     let cardInfo = [
-        {title: "Titulo1", image: firstProjectPhoto, text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, harum voluptate ad fuga eveniet error."},
-        {title: "Titulo2", image: secondProjectPhoto, text: "Bacon ipsum dolor amet kielbasa ground round salami cow. Bresaola jerky pork chop tongue porchetta."},
-        {title: "Titulo3", image: thirdProjectPhoto, text: "Ball tip turkey tenderloin tail pork loin, pastrami buffalo boudin ham hock. Corned beef turkey."},
-        {title: "Titulo4", image: fourtgProjectPhoto, text: "Cow tenderloin tongue cupim jerky beef ribs chicken buffalo pig jowl kielbasa. Alcatra cow brisket."},
+        {title: "Titulo1", imageUrl: firstProjectPhoto, text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, harum voluptate ad fuga eveniet error."},
+        {title: "Titulo2", imageUrl: secondProjectPhoto, text: "Bacon ipsum dolor amet kielbasa ground round salami cow. Bresaola jerky pork chop tongue porchetta."},
+        {title: "Titulo3", imageUrl: thirdProjectPhoto, text: "Ball tip turkey tenderloin tail pork loin, pastrami buffalo boudin ham hock. Corned beef turkey."},
+        {title: "Titulo4", imageUrl: fourtgProjectPhoto, text: "Cow tenderloin tongue cupim jerky beef ribs chicken buffalo pig jowl kielbasa. Alcatra cow brisket."},
      ]
     return (
     <>
         {cardInfo.map((card) =>{
-            return <Card title={card.title} text={card.text} image={card.image}/>
+            return <Card title={card.title} text={card.text} image={card.imageUrl}/>
         })}
     </>
   )
 }
-
-
 export default Cards
